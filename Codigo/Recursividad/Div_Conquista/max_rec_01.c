898871f9f8b2c5c64eaafd4bdeaf7b3a8bcb7120
Carlos Zamora <cdzamora02@outlook.es>
2025-08-04 16:59:05 -0600

    Add code (in C) for compute maximum of array with recursion

---
+++ Codigo/Recursividad/Div_Conquista/max_rec_01.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+#define RED   "\x1B[31m"
+#define GREEN   "\x1B[32m"
+#define RESET "\x1B[0m"
+
+
+int max_array(int *arr, int end)
+{
+    if (end == 0)
+    {
+        return arr[0];
+    }
+
+    int aux = max_array(arr, end - 1);
+
+    //return max(arr[end], aux);
+    return arr[end] > aux ? arr[end] : aux;
+
+    /*
+    if (arr[end] > aux)
+    {
+        return arr[end];
+    }
+    else
+    {
+        return aux;
+    }
+    */
+}
+
+int main()
+{
+    int arr[] = {1, 2, 5, 7, 9, 11};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    if (max_array(arr, size - 1) == 11) 
+        printf(GREEN "Test 1 passed\n" RESET);
+    else
+        printf(RED "Test 1 failed\n" RESET);
+
+    arr[3] = 45;
+    if (max_array(arr, size - 1) == 45) 
+        printf(GREEN "Test 2 passed\n" RESET);
+    else
+        printf(RED "Test 2 failed\n" RESET);
+
+    int arr2[] = {77};
+    size = sizeof(arr2)/sizeof(arr2[0]);
+    if (max_array(arr2, size - 1) == 77) 
+        printf(GREEN "Test 3 passed\n" RESET);
+    else
+        printf(RED "Test 3 failed\n" RESET);
+
+
+    arr[0] = 111;
+    size = sizeof(arr)/sizeof(arr[0]);
+    if (max_array(arr, size - 1) == 111) 
+        printf(GREEN "Test 4 passed\n" RESET);
+    else
+        printf(RED "Test 4 failed\n" RESET);
+
+    arr[1] = 112;
+    size = sizeof(arr)/sizeof(arr[0]);
+    if (max_array(arr, size - 1) == 112) 
+        printf(GREEN "Test 5 passed\n" RESET);
+    else
+        printf(RED "Test 5 failed\n" RESET);
+
+    
+    int arr3[] = {-1, -5, -33, -22, -6};
+    size = sizeof(arr3)/sizeof(arr3[0]);
+    if (max_array(arr3, size - 1) == -1) 
+        printf(GREEN "Test 6 passed\n" RESET);
+    else
+        printf(RED "Test 6 failed\n" RESET);
+
+
+    int arr4[] = {2, 2, 2};
+    size = sizeof(arr4)/sizeof(arr4[0]);
+    if (max_array(arr4, size - 1) == 2) 
+        printf(GREEN "Test 7 passed\n" RESET);
+    else
+        printf(RED "Test 7 failed\n" RESET);
+
+
+}
