32b0e63c08230655c612e23d0433f6aabfc10d2a
Carlos Zamora <cdzamora02@outlook.es>
2025-08-09 10:18:14 -0600

    Add sorting directory and mergesort file (C)

---
+++ Codigo/Ordenamiento/mergesort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Command to generate the executable:
+//      gcc mergesort.c -o mergesort.out
+// Command to run the program:
+//      ./mergesort.out
+
+int main()
+{
+    printf("Hello World");
+}
