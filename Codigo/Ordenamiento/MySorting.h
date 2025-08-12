abaecd6c198c17989ffe6ee74338f2b6a1a6746b
Carlos Zamora <cdzamora02@outlook.es>
2025-08-12 11:32:27 -0600

    Refactor quicksort. Split in different files

---
+++ Codigo/Ordenamiento/MySorting.h
@@ -0,0 +1,53 @@
+#ifndef MY_SORTING_H_INCLUDED
+#define MY_SORTING_H_INCLUDED
+
+// Sort in place the given integer array using quicksort algorithm
+// 
+// If is sorting arr[start:end], choose pivot as median of
+// three elements: arr[start], arr[end], arr[middle]
+// where middle is the middle point between start and end
+// Parmameters:
+//      int *arr: integer pointer to the beginning of the array
+//      int size: size of the given array 
+void quicksort_static_median(int *arr, int size);
+
+
+// Sort in place the given integer array between the range [start: end]
+// 
+// Auxiliar function for quicksort_static_median()
+// Parmameters:
+//      int *arr: integer pointer to the beginning of the array
+//      int start: beginning of the range that is going to be sorted
+//      int end: end of the range that is going to be sorted 
+void quicksorting_static_median(int *arr, int start, int end);
+
+
+// Compute pivot for quicksort algorithm as median of three elements:
+// arr[start], arr[end], arr[middle] where middle is the middle
+// point between start and end
+// Parmameters:
+//      int *arr: integer pointer to the beginning of the array
+//      int start: beginning of the range that is going to be sorted
+//      int end: end of the range that is going to be sorted 
+int compute_pivot_static_median(int *arr, int start, int end);
+
+
+// Do the partition for quicksort algorithm given an integer
+// array between the range [start: end] and a pivot 
+// Parmameters:
+//      int *arr: integer pointer to the beginning of the array
+//      int start: beginning of the range that is going to be sorted
+//      int end: end of the range that is going to be sorted 
+//      int pivot: the pivot of quicksort algorithm
+int partition(int *arr, int start, int end, int pivot);
+
+
+// Compute the median of num_1, num_2 and num_3
+int median_of_3(int num_1, int num_2, int num_3);
+
+
+// Swap values between two integers a and b
+void swap(int *a, int *b);
+
+
+#endif
--- Codigo/Ordenamiento/quicksort.c
+++ Codigo/Ordenamiento/quicksort.c
@@ -1,11 +1,77 @@
-#include <stdio.h>
-
 #include "MySorting.h"
 
-// Command to generate the executable:
-//      gcc quicksort.c -o quicksort.out
-// Command to run the program:
-//      ./quicksort.out
+
+void quicksort_static_median(int *arr, int size)
+{
+    quicksorting_static_median(arr, 0, size - 1);
+}
+
+
+void quicksorting_static_median(int *arr, int start, int end)
+{
+    // Base case: Array only have one element or zero elements
+    if (start >= end)
+    {
+        // Do nothing
+        return;
+    }
+
+    int pivot = compute_pivot_static_median(arr, start, end);
+    int index = partition(arr, start, end, pivot);
+    // index of where is the pivot after partition
+
+    // Call recursively quicksorting_static_median() 
+    if (index - 1 >= start)
+    {
+        quicksorting_static_median(arr, start, index - 1);
+    }
+    if (index + 1 <= end)
+    {
+        quicksorting_static_median(arr, index + 1, end);
+    }
+}
+
+
+int compute_pivot_static_median(int *arr, int start, int end)
+{
+    // Compute middle point of start and end
+    int middle = start + (end-start)/2;
+    // Choose pivot as the median of arr[start], arr[end] and arr[middle]
+    int pivot = median_of_3(arr[start], arr[middle], arr[end]);
+    return pivot;
+}
+
+
+int partition(int *arr, int start, int end, int pivot)
+{
+    int i = start;
+    int j = end;
+    while (i < j)
+    {
+        if (arr[i] < pivot)
+        {
+            // arr[i] is in the right side
+            i++;
+        }
+        else if(arr[j] > pivot)
+        {
+            // arr[j] is in the right side
+            j--;
+        }
+        else
+        {
+            // arr[j] <= pivot <= arr[i]
+            swap(&arr[i], &arr[j]);
+            if (arr[i] == pivot && arr[j] == pivot)
+            {
+                i++;
+            }
+        }
+    }
+
+    return i;
+}
+
 
 int median_of_3(int num_1, int num_2, int num_3)
 {
@@ -49,247 +115,4 @@ void swap(int *a, int *b)
     int tmp = *a;
     *a = *b;
     *b = tmp;
-}
-
-
-int partition(int *arr, int start, int end, int pivot)
-{
-    int i = start;
-    int j = end;
-    while (i < j)
-    {
-        if (arr[i] < pivot)
-        {
-            // arr[i] is in the right side
-            i++;
-        }
-        else if(arr[j] > pivot)
-        {
-            // arr[j] is in the right side
-            j--;
-        }
-        else
-        {
-            // arr[j] <= pivot <= arr[i]
-            swap(&arr[i], &arr[j]);
-            if (arr[i] == pivot && arr[j] == pivot)
-            {
-                i++;
-            }
-        }
-    }
-
-    return i;
-}
-
-
-int compute_pivot_static_median(int *arr, int start, int end)
-{
-    // Compute middle point of start and end
-    int middle = start + (end-start)/2;
-    // Choose pivot as the median of arr[start], arr[end] and arr[middle]
-    int pivot = median_of_3(arr[start], arr[middle], arr[end]);
-    return pivot;
-}
-
-void quicksorting_static_median(int *arr, int start, int end)
-{
-    // Base case: Array only have one element or zero elements
-    if (start >= end)
-    {
-        // Do nothing
-        return;
-    }
-
-    int pivot = compute_pivot_static_median(arr, start, end);
-    int index = partition(arr, start, end, pivot);
-    // index of where is the pivot after partition
-
-    // Call recursively quicksorting_static_median() 
-    if (index - 1 >= start)
-    {
-        quicksorting_static_median(arr, start, index - 1);
-    }
-    if (index + 1 <= end)
-    {
-        quicksorting_static_median(arr, index + 1, end);
-    }
-
-}
-
-
-void quicksort_static_median(int *arr, int size)
-{
-    quicksorting_static_median(arr, 0, size - 1);
-}
-
-int main()
-{
-    int array[] = {2, 3, 1, 8, 6, 1, 2, 11, -7, 7, 4, 2};
-    int size = sizeof(array) / sizeof(array[0]);
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array, size);
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    printf("\n");
-    int array2[] = {8, 6, 1, 2, 11, -7, 7, 4, 2 , 5, 1, -7, -4, 4, -12, 13, 6, 5, -9};
-    int size2 = sizeof(array2) / sizeof(array2[0]);
-    for (int i = 0; i < size2; i++)
-    {
-        printf("%d ", array2[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array2, size2);
-    for (int i = 0; i < size2; i++)
-    {
-        printf("%d ", array2[i]);
-    }
-    printf("\n");
-
-    printf("\n");
-    int array3[] = {7, 6, 5, 4, 3, 2, 1};
-    int size3 = sizeof(array3) / sizeof(array3[0]);
-    for (int i = 0; i < size3; i++)
-    {
-        printf("%d ", array3[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array3, size3);
-    for (int i = 0; i < size3; i++)
-    {
-        printf("%d ", array3[i]);
-    }
-    printf("\n");
-
-    
-    printf("\n");
-    int array4[] = {1, 3, 4, 2, 7, 5, 4, 10, 11, 9, 12, 8};
-    int size4 = sizeof(array4) / sizeof(array4[0]);
-    for (int i = 0; i < size4; i++)
-    {
-        printf("%d ", array4[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array4, size4);
-    for (int i = 0; i < size4; i++)
-    {
-        printf("%d ", array4[i]);
-    }
-    printf("\n");
-
-
-    printf("\n");
-    int array5[] = {8, 8, 8, 8, 8};
-    int size5 = sizeof(array5) / sizeof(array5[0]);
-    for (int i = 0; i < size5; i++)
-    {
-        printf("%d ", array5[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array5, size5);
-    for (int i = 0; i < size5; i++)
-    {
-        printf("%d ", array5[i]);
-    }
-    printf("\n");
-
-
-    printf("\n");
-    int array6[] = {1, 13, -4, 2, -7, 5, -1, 10, -11, 19, -12, 8, 0};
-    int size6 = sizeof(array6) / sizeof(array6[0]);
-    for (int i = 0; i < size6; i++)
-    {
-        printf("%d ", array6[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array6, size6);
-    for (int i = 0; i < size6; i++)
-    {
-        printf("%d ", array6[i]);
-    }
-    printf("\n");
-
-
-    printf("\n");
-    int array7[] = {1, 3, 5, 7, 9, 11, 13, 12, 10, 8, 6, 4, 2};
-    int size7 = sizeof(array7) / sizeof(array7[0]);
-    for (int i = 0; i < size7; i++)
-    {
-        printf("%d ", array7[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array7, size7);
-    for (int i = 0; i < size7; i++)
-    {
-        printf("%d ", array7[i]);
-    }
-    printf("\n");
-
-    printf("\n");
-    int array8[] = {1, -3, 5, -7, 9, -11, 13, -12, 10, -8, 6, -4, 2};
-    int size8 = sizeof(array8) / sizeof(array8[0]);
-    for (int i = 0; i < size8; i++)
-    {
-        printf("%d ", array8[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array8, size8);
-    for (int i = 0; i < size8; i++)
-    {
-        printf("%d ", array8[i]);
-    }
-    printf("\n");
-
-    printf("\n");
-    int array9[] = {1, 7, 1, 7, -12, 7, 7, -12, 1, 7, 1, -12, 1, 7, -12};
-    int size9 = sizeof(array9) / sizeof(array9[0]);
-    for (int i = 0; i < size9; i++)
-    {
-        printf("%d ", array9[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array9, size9);
-    for (int i = 0; i < size9; i++)
-    {
-        printf("%d ", array9[i]);
-    }
-    printf("\n");
-
-    printf("\n");
-    int array10[] = {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0};
-    int size10 = sizeof(array10) / sizeof(array10[0]);
-    for (int i = 0; i < size10; i++)
-    {
-        printf("%d ", array10[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array10, size10);
-    for (int i = 0; i < size10; i++)
-    {
-        printf("%d ", array10[i]);
-    }
-    printf("\n");
-
-    printf("\n");
-    int array11[] = {0, 0, 1, 0, -3, 0, -1, 0, 2, 0, 0, 0, -2, 0, 0, 3};
-    int size11 = sizeof(array11) / sizeof(array11[0]);
-    for (int i = 0; i < size11; i++)
-    {
-        printf("%d ", array11[i]);
-    }
-    printf("\n");
-    quicksort_static_median(array11, size11);
-    for (int i = 0; i < size11; i++)
-    {
-        printf("%d ", array11[i]);
-    }
-    printf("\n");
 }
